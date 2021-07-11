a351267187de741f3d7f9e203a328e8265df7955
masato <maboshiva@gmail.com>
2021-07-12 07:44:46 +0900

    更新

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -745,24 +745,6 @@ background-image : url({{ $microCMS[2]['img']['url'] }})"
                 <p>※2021年4月時点での自社調べ</p>
             </div>
         </div>
-
-        <div class="questionnaire-card">
-            <div class="questionnaire-card-title">
-                <p>投資未経験者・初心者の割合</p>
-            </div>
-            <div class="number">
-                <p><span class="big">90</span><span class="small">％</span></p>
-            </div>
-            <div class="comment">
-                <p>
-                    CashAcademyはこれから学び始める方</p><p>でもカンタンに分かるように作られて</p><p>いるため、挫折せず続けることができ</p><p>ます。
-                </p>
-            </div>
-            <div class="research-day">
-                <p>※2021年4月時点での自社調べ</p>
-            </div>
-        </div>
-
         <div class="questionnaire-card">
             <div class="questionnaire-card-title">
                 <p>20代・30代・40代の割合</p>
@@ -779,6 +761,22 @@ background-image : url({{ $microCMS[2]['img']['url'] }})"
                 <p>※2021年4月時点での自社調べ</p>
             </div>
         </div>
+        <div class="questionnaire-card">
+            <div class="questionnaire-card-title">
+                <p>投資未経験者・初心者の割合</p>
+            </div>
+            <div class="number">
+                <p><span class="big">90</span><span class="small">％</span></p>
+            </div>
+            <div class="comment">
+                <p>
+                    CashAcademyはこれから学び始める方</p><p>でもカンタンに分かるように作られて</p><p>いるため、挫折せず続けることができ</p><p>ます。
+                </p>
+            </div>
+            <div class="research-day">
+                <p>※2021年4月時点での自社調べ</p>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -788,60 +786,118 @@ background-image : url({{ $microCMS[2]['img']['url'] }})"
     </div>
     <div class="short-under-line"></div>
 
+    <div class="sp-question-field">
+    <div >
     <div class="question-answer">
-        <p>
+        <p class="pc-display">
             <span class="question-mark">Q</span
             ><span class="question"
                 >投資や金融などの知識がゼロですが大丈夫でしょうか？</span
             >
         </p>
-        <p class="answer">
-            CashAcademyを受けた方の多くはお金の勉強については初心者です。初めて勉強する方でも理解できるようになっておりますのでご安心ください。
+        <div class="sp-display questions-field" style="display: none;">
+            <p class="question-mark">Q</p
+            >
+            <div style="padding-top: 8%;"><p class="question"
+                >投資や金融などの知識がゼロですが大丈<p><p class="question">夫でしょうか？</p
+            >
+            </div>
+        </div>        
+        <p class="answer pc-display">
+            CashAcademyを受けた方の多くはお金の勉強については初心者です。初めて勉強する方でも理解できるようになっております</p><p class="answer pc-display">のでご安心ください。
+        </p>
+        <p class="answer sp-display" style="display: none;">
+            CashAcademyを受けた方の多くはお金の勉強に</p><p class="answer sp-display" style="display: none;">ついては初心者です。初めて勉強する方でも理</p><p class="answer sp-display" style="display: none;">解できるようになっておりますのでご安心くだ</p><p class="answer sp-display" style="display: none;">さい。
         </p>
     </div>
 
     <div class="question-answer">
-        <p>
+        <p class="pc-display">
             <span class="question-mark">Q</span
             ><span class="question"
                 >保険や投資商品等を売り込まれたりしませんか？</span
             >
         </p>
-        <p class="answer">
-            CashAcademyでは特定の金融機関からお金を頂いて紹介するようなことは一切しておりません。
+        <div class="sp-display questions-field" style="display: none;">
+            <p class="question-mark">Q</p
+            >
+            <div style="padding-top: 8%;"><p class="question"
+                >保険や投資商品等を<p><p class="question">売り込まれたりしませんか？</p
+            >
+            </div>
+        </div>          
+        <p class="answer pc-display">
+            CashAcademyでは特定の金融機関からお金を頂いて紹介するようなことは一切しておりません。</p><p class="answer pc-display">
             お客様がお金の教養を身につけられることを第一優先としています。
+        </p>
+        <p class="answer sp-display" style="display: none;">
+            CashAcademyでは特定の金融機関からお金を頂</p><p class="answer sp-display" style="display: none;">いて紹介するようなことは一切しておりませ</p><p class="answer sp-display" style="display: none;">ん。</p><p class="answer sp-display" style="display: none;">お客様がお金の教養を身につけられることを第</p><p class="answer sp-display" style="display: none;">一優先としています。
         </p>
     </div>
 
     <div class="question-answer">
-        <p>
+        <p class="pc-display">
             <span class="question-mark">Q</span
             ><span class="question">カウンセリングでは何をしますか？</span>
         </p>
-        <p class="answer">
-            ①現状分析、②資産形成のポイント説明、③CashAcademyの紹介をします。
-            マンツーマンで、かつお客様に現状分析を元にお話しするので、多くのお客様に「受けてよかった」という声をいただいております。
+        <div class="sp-display questions-field" style="display: none;">
+            <p class="question-mark">Q</p
+            >
+            <div style="padding-top: 8%;"><p class="question"
+                >カウンセリングでは<p><p class="question">何をしますか？</p
+            >
+            </div>
+        </div>   
+        <p class="answer pc-display">
+            ①現状分析、②資産形成のポイント説明、③CashAcademyの紹介をします。</p><p class="answer pc-display">
+            マンツーマンで、かつお客様に現状分析を元にお話しするので、多くのお客様に「受けてよかった」という声をいただいて</p><p class="answer pc-display">おります。
+        </p>
+        <p class="answer sp-display" style="display: none;">
+            ①現状分析、②資産形成のポイント説明、</p><p class="answer sp-display" style="display: none;">③CashAcademyの紹介をします。</p><p class="answer sp-display" style="display: none;">マンツーマンで、かつお客様に現状分析を元に</p><p class="answer sp-display" style="display: none;">にお話しするので、多くのお客様に「受けてよ</p><p class="answer sp-display" style="display: none;">かった」という声をいただいております。
         </p>
     </div>
 
     <div class="question-answer">
-        <p>
+        <p class="pc-display">
             <span class="question-mark">Q</span
             ><span class="question">カウンセリングは無料ですか？</span>
         </p>
-        <p class="answer">
+        <div class="sp-display questions-field" style="display: none;">
+            <p class="question-mark">Q</p
+            >
+            <div style="padding-top: 8%;"><p class="question"
+                >カウンセリングは<p><p class="question">無料ですか？</p
+            >
+            </div>
+        </div>
+        <p class="answer pc-display">
             はい、無料です。ただし、無料カウンセリングはお1人様1回限りまでの受講とります。
         </p>
+        <p class="answer sp-display" style="display: none;">
+            はい、無料です。ただし、無料カウンセリング</p><p class="answer sp-display" style="display: none;">はお1人様1回限りまでの受講とります。</p>
     </div>
 
     <div class="question-answer">
-        <p>
+        <p class="pc-display">
             <span class="question-mark">Q</span
             ><span class="question">地方にいるのですが受講可能ですか？</span>
         </p>
-        <p class="answer">
+        <div class="sp-display questions-field" style="display: none;">
+            <p class="question-mark">Q</p
+            >
+            <div style="padding-top: 8%;"><p class="question"
+                >地方にいるのですが<p><p class="question">受講可能ですか？</p
+            >
+            </div>
+        </div>
+        <p class="answer pc-display">
             可能です。オンライン（zoom）で行うため、ネット環境と静かな場所があればどこでも受けることが可能です。
         </p>
+        <p class="answer sp-display" style="display: none;">
+            可能です。オンライン（zoom）で行うため、</p><p class="answer sp-display" style="display: none;">ネット環境と静かな場所があればどこでも受け<p class="answer sp-display" style="display: none;">ることが可能です。
+        </p>
     </div>
+    </div>
+ </div>
 </div>
 @component('components.footer') @endcomponent @endsection
