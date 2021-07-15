e658da4c8dc6c03a534c87145dc94306d2142996
masato <maboshiva@gmail.com>
2021-07-16 03:59:59 +0900

    更新

--- resources/views/auth/admin/login.blade.php
+++ resources/views/auth/admin/login.blade.php
@@ -5,8 +5,10 @@
         <form method="POST" action="/admin/login">
             @csrf
             <div style="text-align: center; margin-bottom: 2%">
-                @if ($errors->has('email'))
-                <p class="error-message">{{ $errors->first('email') }}</p>
+                @if ($errors->has('email')||$errors->has('password'))
+                <p class="error-message">
+                    メールアドレスまたはパスワードが間違っております。<br />1分ほどお待ちになった後再度お試してください
+                </p>
                 @endif
             </div>
             <div class="login-email-field">
@@ -16,9 +18,6 @@
             <div class="login-password-field">
                 <label>パスワード</label>
                 <input name="password" value="" type="text" />
-                @if ($errors->has('password'))
-                <p class="error-message">{{ $errors->first('password') }}</p>
-                @endif
             </div>
             <div class="login-button-field">
                 <button type="submit" name="login"><p>ログイン</p></button>
